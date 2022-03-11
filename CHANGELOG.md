e713fe1c69d22b2c43cef83058d66c4a1bce8840
DarrylWitcher <Breach097@gmail.com>
2022-03-11 03:44:15 -0800

    Ignore second commit. Catching up

---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+```
+Thursday, 3/10/2022
+```
+<ul>
+<li>My initial commit of my medical data entry project, Project 1.
+</ul>
+<hr>
+
+```
+Friday, 3/11/2022
+```
+<ul>
+<li>The second commit is the same as the first, my commit and push wasn't showing on GitHub. I added a fake test just to have a file to add.
+<li>Caught up</li>
+<li>Error with database, probably from me not setting it up right</li>
+</ul>
+<hr>
+
+```
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,3 +1,45 @@
-var welcomeMsg = 'Hello, World!'
+var welcomeMsg = 'Medic App';
+document.querySelector('h1').innerText = welcomeMsg;
 
-document.querySelector('h1').innerText = welcomeMsg
+fetch('/patients').then(resp => resp.json()).then(patients => {
+        document.querySelector('#patients').innerHTML = listPatients(patients);
+    }
+);
+
+function listPatients(json) {
+    return `${json.map(listPatient).join('\n')}`;
+};
+
+let listPatient = function(patient) {
+    return '<p>' + patient.patientId + ": " + patient.firstName + '</p>';
+};
+
+function postPatient() {
+    let patient = {
+        "patientId": document.getElementById("patientId").value,
+        "firstName": document.getElementById("firstName").value
+    }
+    console.log(patient);
+    fetch("/patients", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(patient)
+    }).then((result) => {
+        if (result.status != 200) {
+            throw new Error("Bad Server Response");
+        }
+        console.log(result.text());
+    }).catch((error) => { console.log(error); })
+    fetch('/patients').then(resp => resp.json()).then(patients => {
+            document.querySelector('#patients').innerHTML = listPatients(patients);
+        }
+    );
+}
+
+let button = document.querySelector('button');
+button.addEventListener('mouseenter', function() {
+    button.textContent = "Go!";
+})
